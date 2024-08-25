de96271ad171a0aa3b0d3edda393efc7bf9e5a24
Alexey Zherebtsov <alexhitory@gmail.com>
2024-08-25 17:55:24 +0300

    commit 6 sprint task

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,31 @@
+
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+    @Mock
+    Feline feline;
+
+    @Test
+    public void catGetSoundTest() throws Exception {
+        Cat cat = new Cat(feline);
+        assertEquals("Мяу", cat.getSound());
+    }
+
+    @Test
+    public void catGetFoodTest() throws Exception {
+        Cat cat = new Cat(feline);
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        assertEquals(cat.getFood(), List.of("Животные", "Птицы", "Рыба"));
+    }
+}
